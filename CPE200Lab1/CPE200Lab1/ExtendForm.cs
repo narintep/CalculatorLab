8ccc710431c6f8e25132445c7143c4faf6fcf4f7
Narintep <GL@DESKTOP-BBJTHTS>
2017-09-08 17:37:51 +0700

    + - * / Scc

--- CPE200Lab1/CPE200Lab1/ExtendForm.cs
+++ CPE200Lab1/CPE200Lab1/ExtendForm.cs
@@ -15,12 +15,12 @@ namespace CPE200Lab1
         private bool isNumberPart = false;
         private bool isContainDot = false;
         private bool isSpaceAllowed = false;
-        private CalculatorEngine engine;
+        private RPNCalculatorEngine engine;
 
         public ExtendForm()
         {
             InitializeComponent();
-            engine = new CalculatorEngine();
+            engine = new RPNCalculatorEngine();
         }
 
         private bool isOperator(char ch)
@@ -66,6 +66,10 @@ namespace CPE200Lab1
             if (current[current.Length - 1] != ' ')
             {
                 lblDisplay.Text += " " + ((Button)sender).Text + " ";
+                isSpaceAllowed = false;
+            }else
+            {
+                lblDisplay.Text += "" + ((Button)sender).Text + " ";
                 isSpaceAllowed = false;
             }
         }
@@ -163,6 +167,7 @@ namespace CPE200Lab1
             {
                 lblDisplay.Text += " ";
                 isSpaceAllowed = false;
+                isContainDot = false;
             }
         }
     }
